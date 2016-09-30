84cdd545f7a4914e96ab3da69adef2b3dfd70aa9
AntoineMontaut <antoine.montaut@gmail.com>
2016-09-30 15:38:44 -0500

    update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -5,10 +5,12 @@ Unit2 Lesson3: Linear Regression and Correlation
 import pandas as pd
 import plots
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import numpy as np
 
 ds_loans = pd.read_csv("loansData.csv")
 # print(ds_loans.head(5))
-# print(ds_loans.info())
+print(ds_loans.info())
 ds_loans.dropna()
 
 fico = "FICO.Score"
@@ -17,6 +19,10 @@ int_rate = "Interest.Rate"
 int_rate_name = int_rate.replace(".", " ")
 length = "Loan.Length"
 length_name = length.replace(".", " ")
+amt_req = "Amount.Requested"
+amt_req_name = amt_req.replace(".", " ")
+income = "Monthly.Income"
+income_name = income.replace(".", " ")
 
 ds_loans[fico] = ds_loans["FICO.Range"].map(lambda x: int(str(x).rstrip().split("-")[0]))
 ds_loans[int_rate] = ds_loans[int_rate].map(lambda x: float(str(x).rstrip()[:-1])/100)
@@ -40,10 +46,26 @@ def base_data_plots():
     ax3.set_title("Loan Length (months)")
     plt.show()
 
-# base_data_plots()
+base_data_plots()
 
 def scatter_matrix_plot():
-    spm = pd.scatter_matrix(ds_loans, figsize=(10,10), diagonal='hist')
+    # spm = pd.scatter_matrix(ds_loans, figsize=(10,10), diagonal='hist', alpha=0.05)
+    spm_reduced = pd.scatter_matrix(ds_loans[[fico, int_rate, length, amt_req, income]], figsize=(10,10), diagonal='hist', alpha=0.05)
     plt.show()
     
 scatter_matrix_plot()
+
+#y=interest rate, x1=FICO score, x2=Loan amount
+#transpose to have data in a vertical vector form
+y = np.matrix(ds_loans[int_rate]).transpose()
+x1 = np.matrix(ds_loans[fico]).transpose()
+x2 = np.matrix(ds_loans[amt_req]).transpose()
+#create a single matrix from x1 and x2
+x = np.column_stack([x1, x2])
+#add a constant to x to have the full equation: y = a1*x1 + a2*x2 + b
+X = sm.add_constant(x)
+
+#create the linear model and fit
+model = sm.OLS(y, X).fit() #OLS: Ordinary Least Square
+print("\n")
+print(model.summary())
